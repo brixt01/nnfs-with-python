3652a94844aad0913612af72d683cb9bb797223f
brixt01 <brixt01@outlook.com>
2023-12-31 00:25:14 +0100

    [2] Add basic single neuron, using 4 inputs with weights and a single bias

--- Chapter 2/main.py
+++ Chapter 2/main.py
@@ -4,4 +4,10 @@ Author: Ben Brixton
 '''
 
 if __name__ == "__main__":
-    print("Hello world")
+    inputs = [1.0, 2.0, 3.0, 2.5]
+    weights = [0.2, 0.8, -0.5, 1.0]
+    bias = 2.0
+
+    output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + bias
+
+    print(output)
