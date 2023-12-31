3f4c04bcab3ca8a26cd9401236f4408be5646456
brixt01 <brixt01@outlook.com>
2023-12-31 01:49:49 +0100

    [3] Add sample data from nnfs module

--- Chapter 3/main.py
+++ Chapter 3/main.py
@@ -1,11 +1,23 @@
 '''
-Title: Chapter 2
+Title: Chapter 3
 Author: Ben Brixton
 '''
 
 import numpy as np
+from nnfs import nnfs_init, nnfs_spiral_data
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+
+    nnfs_init()     # Set up nnfs module, used for data set
+    x, y = nnfs_spiral_data(100, 3)     # Get sample data: x=data, y=classes
+
+    print(x)
+    print("\n")
+    print(y)
+
+    plt.scatter(x[:,0], x[:,1], c=y, cmap='brg')     # Show sample data
+    plt.show()
     
     inputs = [
         [1.0, 2.0, 3.0, 2.5],       # Sample 1
@@ -13,18 +25,32 @@ if __name__ == "__main__":
         [-1.5, 2.7, 3.3, -0.8],       # Sample 3
     ]
 
-    weights = [
+    # Hidden layer 1
+    weights1 = [
         [0.2, 0.8, -0.5, 1.0],      # Neuron 1
         [0.5, -0.91, 0.26, -0.5],       # Neuron 2
         [-0.26, -0.27, 0.17, 0.87]      # Neuron 3
     ]
-
-    biases = [
+    biases1 = [
         2.0,        # Neuron 1
         3.0,        # Neuron 2
         0.5     # Neuron 3
     ]
 
-    outputs = np.dot(inputs, np.array(weights).T) + biases
+    # Hidden layer 2
+    weights2 = [
+        [0.1, -0.14, 0.5],      # Neuron 1
+        [-0.5, 0.12, -0.33],       # Neuron 2
+        [-0.44, 0.73, -0.13]      # Neuron 3
+    ]
+    biases2 = [
+        -1.0,        # Neuron 1
+        2.0,        # Neuron 2
+        -0.5     # Neuron 3
+    ]
+
+    layer1_outputs = np.dot(inputs, np.array(weights1).T) + biases1
+
+    layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
     
-    print(outputs)
+    print(layer2_outputs)
---
+++ Chapter 3/nnfs.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Initialise module
+def nnfs_init():
+    np.random.seed = 0
+
+# Make and return spiral data
+def nnfs_spiral_data(points, classes):
+    x = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        x[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return x, y
