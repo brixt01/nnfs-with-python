9dd78e9184f59cea0f30486eab042ab43dc8b3c7
brixt01 <brixt01@outlook.com>
2024-01-02 00:50:09 +0100

    Add backward pass for softmax activation

--- Chapter 9/main.py
+++ Chapter 9/main.py
@@ -61,6 +61,17 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)      # Divide by the sum of given sample's outputs
         self.output = probabilities     # Output is a confidence score for each output, summing to 1
 
+    # Backward pass
+    def backward(self, dvalues):
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output and
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
 # (common) loss
 #
 # Used as a base for other loss calculation
