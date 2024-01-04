6910987507b582e737cbaf2300eda37b2cf0fa5b
brixt01 <brixt01@outlook.com>
2024-01-04 17:02:08 +0100

    Fix comment formatting in main file

--- Chapter 11/main.py
+++ Chapter 11/main.py
@@ -7,6 +7,8 @@ from losses import *
 from combinations import *
 from optimisers import *
 
+from matplotlib import pyplot as plt
+
 if __name__ == "__main__":
     
     '''
@@ -20,41 +22,26 @@ if __name__ == "__main__":
     Initialise neural network
     '''
 
-    # Create Dense layer with 2 input features and 64 output values
-    dense1 = Layer_Dense(2, 64)
-    # Create ReLU activation (to be used with Dense layer):
-    activation1 = Activation_ReLU()
-    # Create second Dense layer with 64 input features (as we take output
-    # of previous layer here) and 3 output values (output values)
-    dense2 = Layer_Dense(64, 3)
-    # Create Softmax classifier's combined loss and activation
-    loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
-    # Create optimizer
-    optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-7)
+    dense1 = Layer_Dense(2, 64)     # Dense layer: 2 inputs, 64 neurons
+    activation1 = Activation_ReLU()     # ReLU activation
+    dense2 = Layer_Dense(64, 3)     # Dense layer: 64 inputs, 3 neurons
+    loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()     # Softmax classifier, with loss function
+    optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-7)      # Adaptive momentum optimiser
     
     '''
     Training loop
     '''
     
-    # Train in loop
     for epoch in range(10001):
 
         '''
         Forward pass
         '''
-        # Perform a forward pass of our training data through this layer
+
         dense1.forward(X)
-        # Perform a forward pass through activation function
-        # takes the output of first dense layer here
         activation1.forward(dense1.output)
-        # Perform a forward pass through second Dense layer
-        # takes outputs of activation function of first layer as inputs
         dense2.forward(activation1.output)
-        # Perform a forward pass through the activation/loss function
-        # takes the output of second dense layer here and returns loss
         loss = loss_activation.forward(dense2.output, y)
-        # Calculate accuracy from output of activation2 and targets
-        # calculate values along first axis
 
         '''
         Stats calculations
@@ -65,16 +52,17 @@ if __name__ == "__main__":
             y = np.argmax(y, axis=1)
         accuracy = np.mean(predictions==y)
         if not epoch % 100:
-            print(f'epoch: {epoch}, ' +
-            f'acc: {accuracy:.3f}, ' +
-            f'loss: {loss:.3f}, ' +
-            f'lr: {optimizer.current_learning_rate}')
+            print(f' \
+                  epoch: {epoch} \
+                  acc: {accuracy:.3f} \
+                  loss: {loss:.3f} \
+                  lr: {optimizer.current_learning_rate} \
+            ')
         
         '''
         Backward pass
         '''
-        
-        # Backward pass
+
         loss_activation.backward(loss_activation.output, y)
         dense2.backward(loss_activation.dinputs)
         activation1.backward(dense2.dinputs)
@@ -84,7 +72,6 @@ if __name__ == "__main__":
         Update weights/biases
         '''
 
-        # Update weights and biases
         optimizer.pre_update_params()
         optimizer.update_params(dense1)
         optimizer.update_params(dense2)
